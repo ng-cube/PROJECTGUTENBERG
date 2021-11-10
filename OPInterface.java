6429862d4fe2c3f34ad15e239a2badec621f6fbd
ng-cube <72841956+ng-cube@users.noreply.github.com>
2021-11-10 23:36:38 +0800

    Add files via upload

--- OPInterface.java
+++ OPInterface.java
@@ -1,24 +1,24 @@
 import java.io.IOException;
 
-public interface OPInterface {
+public interface OPInterface{
 
-	boolean add();
-
-	/**
-	 * 
-	 * @param id
-	 */
-	boolean remove(int id);
+	boolean add() throws IOException ;
 
 	/**
 	 * 
 	 * @param id
 	 */
-	boolean edit(int id);
+	boolean remove(int id) throws IOException ;
+
+	/**
+	 * 
+	 * @param id
+	 */
+	boolean edit(int id) throws IOException;
 
 	void display();
 
-	void write() throws IOException;
+	void write() throws IOException ;
 
 	void sort();
 
