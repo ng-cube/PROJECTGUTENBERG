5e2f69ebdfa57636b80055a0ab2cb560d04c20e4
Siteng <65639845+Tangolin@users.noreply.github.com>
2021-11-11 01:34:48 +0800

    Update RRPSS.java

--- RRPSS.java
+++ RRPSS.java
@@ -2,10 +2,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.IOException;
 import java.text.ParseException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.nio.file.Files;
 
 public class RRPSS {
 	static protected MenuManagement menu = new MenuManagement();
-	static protected ArrayList<Table> TableList;
+	static protected ArrayList<Table> TableList = new ArrayList<Table>();
 	static protected BookingManagement booking = new BookingManagement(TableList);
 	static protected StaffManagement staff = new StaffManagement();
 	static protected OrdersManagement order = new OrdersManagement(menu, staff, TableList);
@@ -14,6 +18,21 @@ public class RRPSS {
 
 		Scanner sc = new Scanner(System.in);
 		int user_choice = 0;
+
+		Path path = Paths.get("Table.txt");
+		try (BufferedReader br = Files.newBufferedReader(path)) {
+			String line = br.readLine();
+
+			while (line != null) {
+				String[] tokens = line.split(",");
+				Table table = new Table(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Boolean.parseBoolean(tokens[2]), Boolean.parseBoolean(tokens[3]));
+				TableList.add(table);
+				line = br.readLine();
+			}
+		}
+		catch(IOException e){
+			System.out.println("Corrupted File.");
+		}
 
 		while (true)
 		{
