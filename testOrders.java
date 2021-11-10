6ce6f80bb0bcfe4d99fee31f8455246a087932fe
Jacky <70202722+jackyy823@users.noreply.github.com>
2021-11-11 00:06:44 +0800

    Delete testOrders.java

--- testOrders.java
+++
@@ -1,9 +0,0 @@
-public class testOrders {
-    public static void main(String[] args) {
-        String startDate = "2020-11-23";
-        String endDate = "2020-12-23";
-        System.out.println(new String(new char[87]).replace("\0", "-"));
-        System.out.println("|" + new String(new char[18]).replace("\0", " ") + "Sales Revenue Report from " + startDate + " to " + endDate + new String(new char[18]).replace("\0", " ") + "|");
-        System.out.println(new String(new char[87]).replace("\0", "-"));
-    }
-}
