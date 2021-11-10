bb2950895d2b85e8b5428fcc1d6f593a2a533d65
Jacky <70202722+jackyy823@users.noreply.github.com>
2021-11-10 22:26:42 +0800

    Updated promo and menuManagement

--- MenuManagement.java
+++ MenuManagement.java
@@ -10,10 +10,9 @@ public class MenuManagement implements OPInterface {
 	private Scanner sc = new Scanner(System.in);
 	private ArrayList<MenuItem> Menu = new ArrayList<MenuItem>();
 	private ArrayList<PromoItem> PromoMenu;
-	private boolean return_code;
+	private int globalIndex = 1;
 
 	public MenuManagement() throws IOException {
-
 		Path path = Paths.get("Menu.txt");
 		try (BufferedReader br = Files.newBufferedReader(path)) {
 			String line = br.readLine();
@@ -22,6 +21,7 @@ public class MenuManagement implements OPInterface {
 				String[] tokens = line.split(",");
 				MenuItem.ItemType e = MenuItem.ItemType.valueOf(tokens[3]);
 				MenuItem menuItem = new MenuItem(Integer.parseInt(tokens[0]), tokens[1], tokens[2], e, Float.parseFloat(tokens[4]));
+				globalIndex ++;
 				this.Menu.add(menuItem);
 				line = br.readLine();
 			}
@@ -30,7 +30,33 @@ public class MenuManagement implements OPInterface {
 			e.printStackTrace();
 		}
 
-		// read the menu items from the txt file and instantiate the objects accordingly
+		path = Paths.get("Promo.txt");
+		try (BufferedReader br = Files.newBufferedReader(path)) {
+			String line = br.readLine();
+
+			while (line != null) {
+				String[] tokens = line.split(",");
+				int n = tokens.length;
+				int j = 5;
+
+				MenuItem.ItemType e = MenuItem.ItemType.valueOf(tokens[3]);
+				ArrayList<Integer> itemIDs = new ArrayList<>();
+
+				while (j < n) {
+					itemIDs.add(Integer.parseInt(tokens[j]));
+					j++;
+				}
+
+				PromoItem promoItem = new PromoItem(Integer.parseInt(tokens[0]), tokens[1], tokens[2], e, Float.parseFloat(tokens[4]), itemIDs);
+				globalIndex ++;
+				this.PromoMenu.add(promoItem);
+
+				line = br.readLine();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public boolean add() throws IOException {
@@ -55,7 +81,8 @@ public class MenuManagement implements OPInterface {
 
 		// add object at end of arraylist
 		// instantiate object
-		this.Menu.add(new MenuItem(this.Menu.size()+1, name, description, MenuItem.ItemType.values()[enum_int], price));
+		this.Menu.add(new MenuItem(globalIndex, name, description, MenuItem.ItemType.values()[enum_int], price));
+		globalIndex ++;
 
 		// sort the items by their enum so that they are in the correct order when being printed
 		this.sort();
@@ -64,20 +91,34 @@ public class MenuManagement implements OPInterface {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param id
 	 */
 	public boolean remove(int id) throws IOException{
 		// remove an order from the current order list
-		this.Menu.remove(id);
+		boolean changed = false;
+		for (MenuItem menuItem : Menu) {
+			if (menuItem.getMenuIndex() == id) {
+				Menu.remove(id);
+				changed = true;
+				break;
+			}
+		}
+		for (PromoItem promoItem : PromoMenu) {
+			if (promoItem.getMenuIndex() == id) {
+				PromoMenu.remove(id);
+				changed = true;
+				break;
+			}
+		}
 
 		// also removes it from file (just override with the current list)
 		writeToCsv();
-		return true;
+		return changed;
 	}
 
 	/**
-	 * 
+	 *
 	 * @param id
 	 */
 	public boolean edit(int id) throws IOException {
@@ -134,7 +175,6 @@ public class MenuManagement implements OPInterface {
 	}
 
 	public void display() {
-		// TODO - implement MenuManagement.display
 		System.out.println("\n" + new String(new char[87]).replace("\0", "-"));
 		System.out.println();
 		for (MenuItem menuItem: Menu){
@@ -149,13 +189,7 @@ public class MenuManagement implements OPInterface {
 		}
 	}
 
-	public void write() {
-		// TODO - implement MenuManagement.write
-		throw new UnsupportedOperationException();
-	}
-
 	public void sort() {
-		// TODO - implement MenuManagement.sort
 		int local_id=1;
 		for (MenuItem.ItemType temp: MenuItem.ItemType.values())
 		{
@@ -187,6 +221,40 @@ public class MenuManagement implements OPInterface {
 		}
 	}
 
+	public boolean addItemToPromo(int id) throws IOException {
+		if (!checkPromoSet(id)) {
+			return false;
+		}
+		System.out.println("How many items would you like to add?");
+		int num = sc.nextInt();
+		ArrayList<Integer> arr = PromoMenu.get(id).additem(num);
+		PromoMenu.get(id).setItemIDs(arr);
+		writeToCsv();
+		return true;
+	}
+
+	public boolean removeItemFromPromo(int id) throws IOException {
+		if (!checkPromoSet(id)) {
+			return false;
+		}
+		System.out.println("How many items would you like to remove?");
+		int num = sc.nextInt();
+		ArrayList<Integer> arr = PromoMenu.get(id).removeitem(num);
+		PromoMenu.get(id).setItemIDs(arr);
+		writeToCsv();
+		return true;
+	}
+
+	public boolean checkPromoSet(int id) {
+		// check if unique id is part of promo csv
+		for (PromoItem promoItem : PromoMenu) {
+			if (promoItem.getMenuIndex() == id) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	/**
 	 *
 	 * @param id
@@ -199,9 +267,75 @@ public class MenuManagement implements OPInterface {
 		return this.Menu.size();
 	}
 
-	private void writeToCsv() throws IOException {
-		Path path = Paths.get("Current_Orders.csv");
+	public void displayInterface() throws IOException {
+		System.out.println("\t(1) - CREATE a new Menu item");
+		System.out.println("\t(2) - UPDATE a current Menu item");
+		System.out.println("\t(3) - DELETE a current Menu item");
+		System.out.println("\t(4) - ADD a new Promo item to existing Promo set");
+		System.out.println("\t(5) - REMOVE a Promo item from Promo set");
+
+		int input = sc.nextInt();
+		int id;
+		switch(input) {
+			case 1:
+				if (add()) {
+					System.out.println("Added successfully!");
+				} else {
+					System.out.println("Not added!");
+				}
+				break;
+
+			case 2:
+				System.out.println("Please select an item you would like to edit: ");
+				id = sc.nextInt();
+				if (edit(id)) {
+					System.out.println("Changes made successfully!");
+				} else {
+					System.out.println("Changes were not made.");
+				}
+				break;
+
+			case 3:
+				System.out.println("Please select an item id to delete: ");
+				id = sc.nextInt();
+				if (remove(id)) {
+					System.out.println("Deleted successfully!");
+				} else {
+					System.out.println("Item not deleted.");
+				}
+				break;
+
+			case 4:
+				System.out.println("Please enter the promo set id: ");
+				id = sc.nextInt();
+				if (addItemToPromo(id)) {
+					System.out.println("Added item successfully to promo set!");
+				} else {
+					System.out.println("Item not added.");
+				}
+				break;
+
+			case 5:
+				System.out.println("Please enter the promo set id: ");
+				id = sc.nextInt();
+				if (removeItemFromPromo(id)) {
+					System.out.println("Item successfully removed from promo set!");
+				} else {
+					System.out.println("Item not removed.");
+				}
+				break;
+
+			default:
+				System.out.println("Invalid input!");
+				break;
+		}
+	}
+
+	public void writeToCsv() throws IOException {
+		Path path = Paths.get("Menu.txt");
+		Path path1 = Paths.get("Promo.txt");
 		FileWriter fw = new FileWriter(String.valueOf(path));
+		FileWriter fw1 = new FileWriter(String.valueOf(path));
 
 		for(MenuItem menuItem: this.Menu) {
 			try {
@@ -215,11 +349,28 @@ public class MenuManagement implements OPInterface {
 				e.printStackTrace();
 			}
 		}
+
+		for(PromoItem promoItem: this.PromoMenu) {
+			try {
+				fw1.write(promoItem.getMenuIndex() + ",");
+				fw1.write(promoItem.getName() + ",");
+				fw1.write(promoItem.getDescription() + ",");
+				fw1.write(promoItem.get_menu_type() + ",");
+				fw1.write(promoItem.getPrice() + "");
+				for (int i=0; i<promoItem.getItemIDs().size(); i++) {
+					fw1.write(promoItem.getItemIDs().get(i) + ",");
+				}
+				fw1.write(System.lineSeparator());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 		try {
 			fw.close();
+			fw1.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
--- PromoItem.java
+++ PromoItem.java
@@ -1,21 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PromoItem extends MenuItem {
+	ArrayList<Integer> itemIDs;
+	Scanner sc = new Scanner(System.in);
 
-	private ArrayList<Integer> itemList;
-
-	/**
-	 * 
-	 * @param id
-	 */
-	public boolean additem(int id) {
-
+	public PromoItem(int menuIndex, String name, String description, ItemType e, float price, ArrayList<Integer> itemIDs) {
+		super(menuIndex, name, description, e, price);
+		this.itemIDs = itemIDs;
 	}
 
 	/**
-	 * 
-	 * @param id
+	 * @param num
 	 */
-	public boolean removeitem(int id) {
-
+	public ArrayList<Integer> additem(int num) {
+		System.out.println("Please enter the ids of the items you would like to add:");
+		for (int i=0; i<num; i++) {
+			int id = sc.nextInt();
+			itemIDs.add(id);
+		}
+		return itemIDs;
 	}
 
+	/**
+	 * @param num
+	 */
+	public ArrayList<Integer> removeitem(int num) {
+		System.out.println("Please enter the ids of the items you would like to remove:");
+		for (int i=0; i<num; i++) {
+			int id = sc.nextInt();
+			itemIDs.remove(id);
+		}
+		return itemIDs;
+	}
+
+	public void setItemIDs(ArrayList<Integer> itemIDs) {
+		this.itemIDs = itemIDs;
+	}
+
+	public ArrayList<Integer> getItemIDs() {
+		return this.itemIDs;
+	}
 }
